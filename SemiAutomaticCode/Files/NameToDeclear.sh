2129a90bface5a66e7d1c5302d7af352dcb50d5c
wangxuefeng <wangxuefeng2@xiaoniu66.com>
2019-06-26 16:33:17 +0800

    使gitignore生效

--- SemiAutomaticCode/Files/NameToDeclear.sh
+++ SemiAutomaticCode/Files/NameToDeclear.sh
@@ -2,14 +2,14 @@
 
 awk '{
 	if ($1~/Label/) {
-		print "@property (nonatomic, strong) UILabel *"$1;
+		print "@property (nonatomic, strong) UILabel *"$1";";
 	} else if ($1~/Button/) {
-		print "@property (nonatomic, strong) UIButton *"$1;
+		print "@property (nonatomic, strong) UIButton *"$1";";
 	} else if ($1~/TextField/) {
-		print "@property (nonatomic, strong) UITextField *"$1;
+		print "@property (nonatomic, strong) UITextField *"$1";";
 	} else if ($1~/ImageView/) {
-		print "@property (nonatomic, strong) UIImageView *"$1;
+		print "@property (nonatomic, strong) UIImageView *"$1";";
 	} else {
-		print "@property (nonatomic, strong) UIView *"$1;
+		print "@property (nonatomic, strong) UIView *"$1";";
 	}
 }' $1
